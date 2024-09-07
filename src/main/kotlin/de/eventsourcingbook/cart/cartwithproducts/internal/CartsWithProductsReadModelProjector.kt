240c6574aa637f9ca74d1948152dc4e6981c5c40
Martin Dilger <martin@effectivetrainings.de>
2024-09-08 00:40:48 +0200

    make read model subscribing

--- src/main/kotlin/de/eventsourcingbook/cart/cartwithproducts/internal/CartsWithProductsReadModelProjector.kt
+++ src/main/kotlin/de/eventsourcingbook/cart/cartwithproducts/internal/CartsWithProductsReadModelProjector.kt
@@ -6,48 +6,50 @@ import de.eventsourcingbook.cart.events.CartClearedEvent
 import de.eventsourcingbook.cart.events.ItemAddedEvent
 import de.eventsourcingbook.cart.events.ItemArchivedEvent
 import de.eventsourcingbook.cart.events.ItemRemovedEvent
-import java.util.UUID
+import org.axonframework.config.ProcessingGroup
 import org.axonframework.eventhandling.EventHandler
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.stereotype.Component
+import java.util.UUID
 
-interface CartsWithProductsReadModelRepository :
-    JpaRepository<CartsWithProductsReadModelEntity, CartProduct> {
+interface CartsWithProductsReadModelRepository : JpaRepository<CartsWithProductsReadModelEntity, CartProduct> {
+    fun findByProductId(productId: UUID): List<CartsWithProductsReadModelEntity>
 
-  fun findByProductId(productId: UUID): List<CartsWithProductsReadModelEntity>
-
-  @Modifying fun deleteAllByAggregateId(cartId: UUID): List<CartsWithProductsReadModelEntity>
+    @Modifying fun deleteAllByAggregateId(cartId: UUID): List<CartsWithProductsReadModelEntity>
 }
 
+@ProcessingGroup("archive-item")
 @Component
-class CartsWithProductsReadModelProjector(var repository: CartsWithProductsReadModelRepository) {
+class CartsWithProductsReadModelProjector(
+    var repository: CartsWithProductsReadModelRepository,
+) {
+    @EventHandler
+    fun on(event: CartClearedEvent) {
+        // throws exception if not available (adjust logic)
+        this.repository.deleteAllByAggregateId(event.aggregateId)
+    }
 
-  @EventHandler
-  fun on(event: CartClearedEvent) {
-    // throws exception if not available (adjust logic)
-    this.repository.deleteAllByAggregateId(event.aggregateId)
-  }
+    @EventHandler
+    fun on(event: ItemRemovedEvent) {
+        // throws exception if not available (adjust logic)
+        this.repository.deleteById(CartProduct(event.aggregateId, event.itemId))
+    }
 
-  @EventHandler
-  fun on(event: ItemRemovedEvent) {
-    // throws exception if not available (adjust logic)
-    this.repository.deleteById(CartProduct(event.aggregateId, event.itemId))
-  }
+    @EventHandler
+    fun on(event: ItemArchivedEvent) {
+        // throws exception if not available (adjust logic)
+        this.repository.deleteById(CartProduct(event.aggregateId, event.itemId))
+    }
 
-  @EventHandler
-  fun on(event: ItemArchivedEvent) {
-    // throws exception if not available (adjust logic)
-    this.repository.deleteById(CartProduct(event.aggregateId, event.itemId))
-  }
-
-  @EventHandler
-  fun on(event: ItemAddedEvent) {
-    // throws exception if not available (adjust logic)
-    repository.save(
-        CartsWithProductsReadModelEntity().apply {
-          this.productId = event.productId
-          this.aggregateId = event.aggregateId
-        })
-  }
+    @EventHandler
+    fun on(event: ItemAddedEvent) {
+        // throws exception if not available (adjust logic)
+        repository.save(
+            CartsWithProductsReadModelEntity().apply {
+                this.productId = event.productId
+                this.aggregateId = event.aggregateId
+            },
+        )
+    }
 }
