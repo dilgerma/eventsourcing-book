2936b0419b80dbfd10ab83724fb803b7e4e286c1
Martin Dilger <martin@effectivetrainings.de>
2024-09-15 08:46:31 +0200

    bugfix - change price needs target aggregate identifier

--- src/main/kotlin/de/eventsourcingbook/cart/domain/commands/changeprice/ChangePriceCommand.kt
+++ src/main/kotlin/de/eventsourcingbook/cart/domain/commands/changeprice/ChangePriceCommand.kt
@@ -1,12 +1,11 @@
 package de.eventsourcingbook.cart.domain.commands.changeprice
 
 import org.axonframework.modelling.command.TargetAggregateIdentifier
-import de.eventsourcingbook.cart.common.Command
 import java.util.UUID
 
-
 data class ChangePriceCommand(
-	var productId:UUID,
-    var newPrice:Double,
-	var oldPrice:Double,
+    @TargetAggregateIdentifier
+    var productId: UUID,
+    var newPrice: Double,
+    var oldPrice: Double,
 )
